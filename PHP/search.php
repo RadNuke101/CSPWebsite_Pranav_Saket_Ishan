d048adc42249bc2d66abf18e40471331737ca03d
10024564 <10024564@american.fmtusd.org>
2021-12-16 15:13:02 -0800

    styled search, add links to dropdown menu

--- PHP/search.php
+++ PHP/search.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../CSS/search.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="../CSS/search.css">
     <title>Search</title>
 </head>
 
