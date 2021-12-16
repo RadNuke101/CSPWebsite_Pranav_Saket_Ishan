698f7a7cef7eb982d67157360b53a2fdd9130743
10024564 <10024564@american.fmtusd.org>
2021-12-16 15:10:57 -0800

    search works now just need to style it

--- PHP/search.php
+++ PHP/search.php
@@ -7,28 +7,49 @@
     <link rel="stylesheet" type="text/css" href="../CSS/search.css">
     <title>Search</title>
 </head>
+
 <body>
+<div class="w3-bar w3-black">
+  <a href="Home_Page.htm" class="w3-bar-item w3-button">Home</a>
+  <a href = "heroes.php" class = "w3-bar-item w3-button">Heroes</a>
+  <div class="w3-dropdown-hover">
+    <button class="w3-button">Marvel</button>
+    <div class="w3-dropdown-content w3-bar-block w3-card-4">
+      <a href="Marvel_Cinema.htm" class="w3-bar-item w3-button">Cinema</a>
+      <a href="Marvel_Timeline.htm" class="w3-bar-item w3-button">Timeline</a>
+    </div>
+  </div>
+  <div class="w3-dropdown-hover">
+    <button class="w3-button">DC</button>
+    <div class="w3-dropdown-content w3-bar-block w3-card-4">
+      <a href="DC_Cinema.htm" class="w3-bar-item w3-button">Cinema</a>
+      <a href="DC_Timeline.htm" class="w3-bar-item w3-button">Timeline</a>
+    </div>
+  </div>
+</div>
+
+
 <?php
     $user = 'root';
         $password = "";
-        $db = "characters";
+        $db = "heroes";
 
         $db = new mysqli('localhost', $user, $password, $db) or die("unable to connect");
 
         // echo "database connected";
-    $username = mysqli_real_escape_string($db, $_GET['query']);
-    $sql = "SELECT id, name, universe FROM IsSecret WHERE team='Hero'";
+    $query = mysqli_real_escape_string($db, $_GET['query']);
+    $sql = "SELECT * FROM characters WHERE Hero LIKE'%$query%'";
     $result = $db->query($sql);
 
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. "<br>";
+        echo "Hero: " . $row["Hero"]. "<br>";
     }
     } else {
     echo "0 results";
     }
-    $conn->close();
+    $db->close();
     ?>
 </body>
 </html>
